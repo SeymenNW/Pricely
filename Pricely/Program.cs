cdd8ba009be6dbdc1c56d0cac207b788aecb3a5e
seymennw <seymennw@gmail.com>
2024-10-26 22:01:42 +0200

    Pricely Blazor projektet skiftes ud. Mangler et par ting..

--- Pricely/Program.cs
+++ Pricely/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using Pricely;
+using PricelyOld;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
