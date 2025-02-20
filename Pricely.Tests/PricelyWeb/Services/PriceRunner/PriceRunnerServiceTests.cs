277e11e24f3acdd73f68468053ed6d76f480b310
Seymen <seymennw@gmail.com>
2025-02-20 12:20:14 +0100

    Added xUnit project

---
+++ Pricely.Tests/PricelyWeb/Services/PriceRunner/PriceRunnerServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pricely.Tests.PricelyWeb.Services.PriceRunner
+{
+
+    //Not finished yet
+    public class PriceRunnerServiceTests
+    {
+
+
+        [Fact]
+        public void X()
+        {
+
+        }
+    }
+}
---
+++ Pricely.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace Pricely.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+        }
+    }
+}
