8cd199ef39e66b2863aa80d2612ac58e75fe05b0
seymen <seymennw@gmail.com>
2025-04-12 14:13:09 +0200

    Added comparison site folders

---
+++ Pricely.Core/Services/ComparisonSites/ComparisonSite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pricely.Core.Services.ComparisonSites
+{
+    public abstract class ComparisonSite
+    {
+
+    }
+}
