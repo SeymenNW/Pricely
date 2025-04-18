1e3eea4047b6c45bd356d25f80bfd4df5982e2d5
seymen <seymennw@gmail.com>
2025-04-18 16:12:00 +0200

    added tailwind + experimental

--- src/Pricely.Frontend/pricely.client/src/app/app.component.ts
+++ src/Pricely.Frontend/pricely.client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, FloatLabelModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
